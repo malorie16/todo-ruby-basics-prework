1f5a4cda9514d0388b897f8ce6d282584c58d9e4
malorie16 <malorie18@gmail.com>
2018-04-30 19:19:13 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  print value
 end
 
-def argue
-  
+def argue(arg)
+  arg
 end
 
-def greeting
-
-end
-
-def return_a_value
+def greeting(greeting, name)
 
 end
 
-def last_evaluated_value
-
+def return_a_value(phrase)
+  phrase = "Nice"
+  phrase
 end
 
-def pizza_party
+def last_evaluated_value(phrase)
+  phrase = "expert"
+  phrase
+end
 
+def pizza_party(ing = "cheese")
+  ing
 end
